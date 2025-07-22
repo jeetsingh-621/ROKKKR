b0398e7bab502123e9c56315d0788132fd12f23e
jeetisngh-621 <jeetsinghjeet68@gmail.com>
2025-07-22 17:09:31 +0530

    added missing video file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import signupbg from "../assets/videos/klvideo5.mp4"; 
+import signupbg from "../../public/videos/klvideo5.mp4"; 
 import { Link } from "react-router-dom";
 
 const Signup = () => {
